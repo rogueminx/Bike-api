5d6846ee5364556f8024cd882d0a37024008ccda
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-21 15:05:43 -0700

    list now has links to detail page, and posts users photos.

--- src/bike.js
+++ src/bike.js
@@ -5,7 +5,9 @@ export function Bike(location) {
     $('.show-text').text(`The following bikes have been stolen in your area:`);
     for(let i = 0; i < response.bikes.length; i++) {
       let bike = response.bikes[i].title;
-      $('#showBikes').append(" " + '<li>' + bike + '</li>');
+      let bike_id = response.bikes[i].id;
+      let bike_thumb = response.bikes[i].thumb;
+      $('#showBikes').append(`<li><a href="https://bikeindex.org:443/api/v3/bikes/${bike_id}."> ${bike} </a><img src ="${bike_thumb}"></li>`);
     }
     }).fail(function(error) {
       $('#showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
