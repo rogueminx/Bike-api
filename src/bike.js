50934dc111394390e84fbcab9db64e0075a2429d
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-03-22 16:52:15 -0700

    add promises and seperate back end and front end more fully.

--- src/bike.js
+++ src/bike.js
@@ -1,34 +1,37 @@
-import $ from 'jquery';
 
 const bikeKey = process.env.BIKE_INDEX_API_KEY;
 
 export class Bike {
-  constructor(location) {
-  this.location = location;
- }
 
-  allBikes() {
-    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${this.location}&distance=10&api_key=${bikeKey}&stolenness=proximity`).then(function(response) {
-      $('.show-text').text(`The following bikes have been stolen in your area:`);
-      for(let i = 0; i < response.bikes.length; i++) {
-        let bike = response.bikes[i].title;
-        let bike_id = response.bikes[i].id;
-        let bike_thumb = response.bikes[i].thumb;
-        if (bike_thumb === null) {
-          bike_thumb = "images/bike-placeholder.png";
+  allBikes(location) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&api_key=${bikeKey}&stolenness=proximity`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
         }
-        $('#showBikes').append(`<div class="row" id="resultrow"><div class="col-md-8"><li><span id="bikedetails" data-custom-value="${bike_id}"> ${bike} </span></div><div class="col-md-2"><img src ="${bike_thumb}"></li></div></div>`);
       }
-    }).fail(function(error) {
-      $('#showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-    })
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 
   bikeInfo(bike_id) {
-    $.get(`https://bikeindex.org:443/api/v3/bikes/${bike_id}`).then(function(response) {
-      let bike = response.bike.title;
-      $('.bikename').text(`${bike}`);
-    })
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://bikeindex.org:443/api/v3/bikes/${bike_id}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
-
 }
--- src/main.js
+++ src/main.js
@@ -4,19 +4,43 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Bike } from './bike.js';
 
+
 $(document).ready(function() {
   $('#searchForBikes').click(function() {
-    event.preventDefault();
-    const location = $('#location').val();
-    let bikes = new Bike(location)
-    bikes.allBikes();
-  })
-  $(document).on('click', '#bikedetails', function(){
-    let bike_id = $(this).data('custom-value');
-    let bike = new Bike(location);
-    bike.bikeInfo(bike_id);
-    $('#showBikes').hide();
-    $("#bikeDetailPage").show();
-  })
-
-});
+      event.preventDefault();
+      const location = $('#location').val();
+      let bike = new Bike();
+      let promise = bike.allBikes(location);
+      promise.then(function(response) {
+        let results = JSON.parse(response);
+        for(let i = 0; i < results.bikes.length; i++) {
+          let bike = results.bikes[i].title;
+          let bike_id = results.bikes[i].id;
+          let bike_thumb = results.bikes[i].thumb;
+          if (bike_thumb === null) {
+            bike_thumb = "images/bike-placeholder.png";
+          }
+          $('.show-text').text(`The following bikes have been stolen in your area:`);
+          $('#showBikes').append(`<div class="row" id="resultrow"><div class="col-md-8"><li><span id="bikedetails" data-custom-value="${bike_id}"> ${bike} </span></div><div class="col-md-2"><img src ="${bike_thumb}"></li></div></div>`);
+        }
+      })
+    })
+    $(document).on('click', '#bikedetails', function(){
+      $('#showBikes').hide();
+      $('#bikeDetailPage').show();
+      let bike_id = $(this).data('custom-value');
+      let singlebike = new Bike();
+      let promiseSecond = singlebike.bikeInfo(bike_id);
+      promiseSecond.then(function(response) {
+        let results = JSON.parse(response);
+        let bike = results.bike.title;
+        let description = results.bike.description;
+        for (let i = 0; i < results.bike.public_images.length; i++) {
+          let image = results.bike.public_images[i].full;
+          $('.bikephotos').append(`<img src ="${image}">`);
+        }
+        $('.bikename').text(`${bike}`);
+        $('.bikedescription').text(`${description}`);
+      })
+    })
+  });
